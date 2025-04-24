21242b2d9fcc9be74be15777695a8d717ad104ca
Omar Mohamed <omarmohamed@192.168.1.4>
2025-04-24 04:07:16 +0200

    Remove deprecated scripts from Git tracking to reduce token count

--- deprecated_scripts/debug_log.py
+++
@@ -1,9 +0,0 @@
-import logging
-import traceback
-import sys
-
-def log_exception():
-    """Log detailed exception information."""
-    exc_type, exc_value, exc_traceback = sys.exc_info()
-    tb_lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
-    logging.error("Exception details:\n" + "".join(tb_lines))
